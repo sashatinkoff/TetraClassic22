65d0ff089290b7efacdec9c693fefc63734c5b10
Aleksandr Tamakov <sashatinkoff@gmail.com>
2022-11-16 11:46:28 +0500

    Sample Worker

--- app/src/main/java/com/isidroid/b21/ui/main/MainViewModel.kt
+++ app/src/main/java/com/isidroid/b21/ui/main/MainViewModel.kt
@@ -3,13 +3,11 @@ package com.isidroid.b21.ui.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.isidroid.b21.domain.usecase.SessionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val sessionUseCase: SessionUseCase
 ) : ViewModel() {
     private val _viewState = MutableLiveData<State>()
     val viewState: LiveData<State> get() = _viewState
